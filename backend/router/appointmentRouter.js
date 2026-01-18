b0c353bcdff60a02e86bfe1a968459d5555424db
Rit005 <rithiksharma006@gmail.com>
2026-01-18 19:15:44 +0530

    main

--- backend/router/appointmentRouter.js
+++ backend/router/appointmentRouter.js
@@ -1,12 +1,18 @@
-import { deleteAppointment, getAllAppointments, postAppointment, updateAppointmentStatus } from "../controller/appointmentController.js";
 import express from "express";
+import {
+  deleteAppointment,
+  getAllAppointments,
+  postAppointment,
+  updateAppointmentStatus
+} from "../controller/appointmentController.js";
 import { isAdminAuthenticated, isPatientAuthenticated } from "../middlewares/auth.js";
+
 const router = express.Router();
 
-router.post("/post", createAppointment);
+router.post("/post", isPatientAuthenticated, postAppointment);
 
-router.get("/getall",isAdminAuthenticated,getAllAppointments)
-router.put("/update/:id",isAdminAuthenticated,updateAppointmentStatus)
-router.delete("/delete/:id",isAdminAuthenticated,deleteAppointment)
+router.get("/getall", isAdminAuthenticated, getAllAppointments);
+router.put("/update/:id", isAdminAuthenticated, updateAppointmentStatus);
+router.delete("/delete/:id", isAdminAuthenticated, deleteAppointment);
 
 export default router;
